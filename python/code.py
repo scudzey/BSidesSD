9c37609fefd5c7b93503ac3d5eff2a2082101d06
Scudzey <scudzey@gmail.com>
2020-03-07 13:26:51 -0800

    Add additional Animation
    Moved constants to Defines.py
    Refactored main entrypoint
    Added everyOtherAnimation to defined the function
    to pulse every other LED

--- python/code.py
+++ python/code.py
@@ -2,21 +2,35 @@ import board
 import neopixel
 import adafruit_fancyled.adafruit_fancyled as fancy
 import time
+import random
 
-NUM_LEDS = 5
-pixels = neopixel.NeoPixel(board.APA102_MOSI, NUM_LEDS, auto_write=False, brightness=0.1)
+import Defines
 
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
-color1 = fancy.CHSV(0.08, 1.0, 1.0).pack()
+from everyOtherAnimation import everyOtherPulse
 
-while True:
-    for i in range(256):
-        color = fancy.CHSV(i,255,255).pack()
-        pixels.fill(color)
-        pixels.show()
-        time.sleep(0.01)
+pixels = neopixel.NeoPixel(board.APA102_MOSI, Defines.NUM_LEDS, auto_write=False, brightness=0.1)
+
+
+
+def main():
+    pixels.fill(Defines.COLOR_CLEAR)
+    pixels.show()
+    ledSelects = 0
+    while True:
+    
+    
+        randomHue = random.randint(0,255)
+        everyOtherPulse(pixels, randomHue, ledSelects)
+        ledSelects = (ledSelects + 1) % 2
+    
+        '''
+        Original LED pattern
+
+        for i in range(256):
+            color = fancy.CHSV(40,255,i).pack()
+            pixels.fill(color)
+            pixels.show()
+            time.sleep(0.01)
+        '''
+
+main()
